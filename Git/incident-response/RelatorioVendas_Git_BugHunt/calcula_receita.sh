ed80f4f4a00a401675199557c40591c3b1021901
Ciro <cirojunioteixeira1@gmail.com>
2025-10-09 09:26:14 -0300

    refactor: Otimiza script para ignorar vendas de alto valor (outliers)

--- Git/incident-response/RelatorioVendas_Git_BugHunt/calcula_receita.sh
+++ Git/incident-response/RelatorioVendas_Git_BugHunt/calcula_receita.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script para calcular a receita total das vendas
-tail -n +2 dados/vendas.csv | cut -d',' -f3 | paste -sd+ | bc
+# Script para calcular a receita total das vendas (exclui outliers)
+tail -n +2 dados/vendas.csv | cut -d',' -f3 | awk '$1 < 1000' | paste -sd+ | bc
